7f0b98a364ea04917e69953960a585ca40d0846b
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-25 04:51:00 +0600

    Implement footer section items.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,21 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import h_img from '../../../images/bg-image.png'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <img src={h_img} alt="" />
+        <div className='flex flex-row  items-center justify-center '>
+            <img src={h_img} alt="" className='w-full relative'/>
+            <div className='shadow-md shadow-neutral-500 p-16 absolute bg-slate-100 '>
+                <div className=' text-center '>
+                    <h3 className=' font-bold'>The natural candle</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor eaque aperiam repudiandae, nesciunt minus fugiat vel excepturi expedita? Ea, animi!</p>
+                </div>
+                <div className='flex items-center justify-center'>
+                    <Link className='bg-green-400  rounded-md  p-2 m-2'>Discover our collection</Link>
+                </div>
+            </div>
         </div>
     );
 };
--- src/NavItems.jsx
+++ src/NavItems.jsx
@@ -1,15 +1,26 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
 
 const NavItems = () => {
+    const [open, setOpen] = useState(false);
     return (
-        <div className='flex items-center justify-between p-8 bg-'>
+        <nav className='flex items-center justify-between p-8 bg-neutral-100 sticky top-0 z-50 md:text-2xl'>
+            <div onClick={() => { setOpen(!open) }} className='md:hidden'>
+                <span>
+                    {
+                        open === true ? <XMarkIcon className='h-10 w-10'></XMarkIcon> : <Bars3Icon className='h-10 w-10'></Bars3Icon>
+                    }
+                </span>
+            </div>
+
             <div className='flex items-center gap-4'>
                 <div>icon</div>
                 <Link>CandLeaf</Link>
             </div>
-            <div className='flex items-center gap-4'>
+
+            <div className={`md:flex items-center gap-4 duration-700 md:opacity-100 ${open ? ' opacity-100' : ' opacity-0'}`}>
                 <Link>Discovery</Link>
                 <Link to='/about'>About</Link>
                 <Link>Contact us</Link>
@@ -18,7 +29,7 @@ const NavItems = () => {
                 <div>Login icon</div>
                 <div>Cart Icon</div>
             </div>
-        </div>
+        </nav>
     );
 };
 
