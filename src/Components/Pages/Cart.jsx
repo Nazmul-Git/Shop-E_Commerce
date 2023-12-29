fd24c682ada4d3ca860202e08373af7dafc6b399
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-12-30 03:02:44 +0600

    Implement add cart.

--- src/Components/Pages/Cart.jsx
+++ src/Components/Pages/Cart.jsx
@@ -5,9 +5,19 @@ import { useLoaderData } from 'react-router-dom';
 const Cart = () => {
     const product=useLoaderData();
     console.log(product);
+    const {_id,name,price, imgUrl,details,mostSell}=product
     return (
-        <div>
-            <p>Cart</p>
+        <div className=' grid grid-cols-2 p-8 m-8 gap-10'>
+            <div className=' bg-gray-100 p-4'>
+                <img src={imgUrl} alt="" className=''/>
+                <p>All hand-made with natural soy wax, Candleaf is made for your pleasure moments.  
+                   🚚 FREE SHIPPING</p>
+            </div>
+            <div className=' shadow-md shadow-blue-500 p-4'>
+                <p>{name}</p>
+                <p>$ {price}</p>
+                <p>Quantity</p>
+            </div>
         </div>
     );
 };
