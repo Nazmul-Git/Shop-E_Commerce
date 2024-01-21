3fa9cf425be05562b7a444a4bc16c910a493d6b2
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2024-01-22 03:52:25 +0600

    Implement order summery on user info form.

--- src/PlaceOrder.jsx
+++ src/PlaceOrder.jsx
@@ -1,29 +1,33 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
+import { Input } from 'postcss';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const PlaceOrder = () => {
+    const [isUser, setUser] = useState(false);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const form=e.target;
-        const name=form.name.value;
-        const number=form.number.value;
-        const province=form.province.value;
-        const city=form.city.value;
-        const area=form.area.value;
-        const address=form.address.value;
+        const form = e.target;
+        const name = form.name.value;
+        const number = form.number.value;
+        const province = form.province.value;
+        const city = form.city.value;
+        const area = form.area.value;
+        const address = form.address.value;
         // console.log(name,number, province,city,area,address)
+        if (form) setUser(true);
+        form.reset();
     }
 
     return (
-        <div className=' grid grid-cols-2 p-20'>
+        <div className=' md:grid grid-cols-2 lg:p-20 md:p-8 gap-4'>
             <div>
                 <div className=' text-black'>
                     <div className="">
                         <div className="hero-content flex-col backdrop-blur-sm">
                             <div className="text-center ">
-                                <h1 className="text-xl font-bold text-black font-serif">User Info</h1>
+                                <h1 className="text-xl font-bold font-serif text-green-600">User Info</h1>
                             </div>
                             <div className="mt-10">
                                 <form onSubmit={handleSubmit} className=" flex flex-col gap-4">
@@ -66,24 +70,54 @@ const PlaceOrder = () => {
                                         </label>
                                         <input type="text" name='address' required placeholder="Address" className="input input-bordered" />
                                     </div>
-                                    
-                                    
-                                    <div className="mt-6">
-                                        <button  type='submit' value='submit' className=" text-lg font-bold bg-orange-400 p-2 w-full rounded-md"> Save</button>
+
+                                    <div className="">
+                                        <button type='submit' value='submit' className=" text-lg font-bold bg-orange-300 p-2 w-full rounded-md hover:bg-orange-500 md:mt-20 "> Save</button>
                                     </div>
-
                                 </form>
-                                <div>
-
-                                </div>
                             </div>
                         </div>
                     </div>
                 </ div>
             </div>
-            <div>
-                <p>total</p>
+
+            <div className={`${isUser ? 'opacity-100' : 'opacity-0'} shadow-md
+                shadow-orange-600 rounded-md duration-700 md:p-4`}>
+                <div className='md:mt-10'>
+                    <p className=' text-md font-bold'>Discount & Payment</p>
+                    <div className='grid grid-cols-2 mt-2'>
+                        <div className=' flex flex-col gap-2'>
+                            <p>Voucher</p>
+                            <p>Promo Code</p>
+                        </div>
+                        <div className=' flex flex-col gap-2'>
+                            <p className=' underline text-sm'>Voucher is not Available</p>
+                            <input type='text' className=' bg-slate-100 rounded-md'></input>
+                        </div>
+                    </div>
+                </div>
+                <div className=' mt-4'>
+                    <p className=' text-md font-bold'>Order Summery</p>
+                    <div className='grid grid-cols-2 mt-2'>
+                        <div className=' flex flex-col gap-2'>
+                            <p>Items Total</p>
+                            <p>Delivery Fee</p>
+                            <p>Delivery Discount</p>
+                            <p>Total Payment</p>
+                        </div>
+                        <div className=' flex flex-col gap-2 text-red-600 font-semibold'>
+                            <p>100</p>
+                            <p>55</p>
+                            <p>55</p>
+                            <p>157</p>
+                            <Link className=' bg-blue-400 hover:bg-blue-600 p-2 text-center text-white font-bold rounded-md '>Place-Order</Link>
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
+
         </div>
     );
 };
